7677ba34ab4986d3a8fa2914edc1f0dfcc884a7d
Anastasiia Moskvina <anastasiiamoskvina@Anastasiias-MacBook-Pro.local>
2022-12-18 23:23:57 +0400

    update ls

--- fsOperations/ls.js
+++ fsOperations/ls.js
@@ -1,32 +1,25 @@
 import path from 'path'
-import fsPromises from 'fs/promises';
-import { EXECUTION_ERROR_MESSAGE } from '../constants.js';
-
-const getFormattedEntries = (entries, type) => {
-    return entries.map(res => res.value).filter(entry => entry.Type === type).sort()
-}
+import fsPromises from 'fs/promises'
+import { EXECUTION_ERROR_MESSAGE } from '../constants.js'
 
 export const ls = async () => {
     try {
-        const promises = [];
-        const entries = await fsPromises.readdir(path.resolve(), 'utf8');
-
-        entries.forEach(entry => {
-            const promise = new Promise(async (res, _rej) => {
-                const stat = await fsPromises.lstat(path.resolve(entry));
-                if (stat.isDirectory()) {
-                    res({ Name: entry, Type: 'directory' })
-                } else {
-                    res({ Name: entry, Type: 'file' })
-                }
-            })
-            promises.push(promise)
+        const entries = await fsPromises.readdir(path.resolve(), {
+            withFileTypes: true,
         })
-        const results = await Promise.allSettled(promises);
-        const directories = getFormattedEntries(results, 'directory');
-        const files = getFormattedEntries(results, 'file');
-        console.table([...directories, ...files]);
+
+        const directories = entries
+            .filter((entry) => entry.isDirectory())
+            .map(({ name }) => ({ Name: name, Type: 'directory'}))
+            .sort()
+
+        const files = entries
+            .filter((entry) => entry.isFile())
+            .map(({ name }) => ({ Name: name, Type: 'file'}))
+            .sort()
+
+        console.table([...directories, ...files])
     } catch (_err) {
-        throw new Error(EXECUTION_ERROR_MESSAGE);
+        throw new Error(EXECUTION_ERROR_MESSAGE)
     }
 }
